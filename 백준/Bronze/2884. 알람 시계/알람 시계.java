a7f228a4da3161028c8c51def24ac496445b93f9
sukhadukkha <parkjiho1229@naver.com>
2025-06-03 22:48:57 +0900

    [Bronze III] Title: 알람 시계, Time: 188 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+        if (minute < 45) {
+            hour--;
+            if (hour < 0) {
+                hour = 23;
+            }
+            minute = (60 + minute) - 45;
+           
+        } else {
+            minute = minute - 45;
+        }
+        System.out.println(hour + " " + minute);
+    }
+}
