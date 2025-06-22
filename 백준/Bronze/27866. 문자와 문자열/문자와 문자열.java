45fade0b503c2098f456bb6a3cce313df284a9cf
sukhadukkha <parkjiho1229@naver.com>
2025-06-22 23:28:54 +0900

    [Bronze V] Title: 문자와 문자열, Time: 188 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        int i = scanner.nextInt();
+        System.out.println(s.charAt(i-1));
+    }
+}
