8501e131363ad5fc675f287985efc2381341417c
sukhadukkha <parkjiho1229@naver.com>
2025-07-02 21:08:56 +0900

    [Bronze III] Title: 웰컴 키트, Time: 136 ms, Memory: 16064 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] sizes = new int[6];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 6; i++) {
+            sizes[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int tCnt = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            tCnt += sizes[i] / T;
+            if (sizes[i] % T > 0) {
+                tCnt += 1;
+            }
+        }
+        System.out.println(tCnt);
+        System.out.println(n / P + " " + n % P);
+    }
+}
