2507cd0652111fa05b3aec3617da1f15009d02e1
sukhadukkha <parkjiho1229@naver.com>
2025-09-01 18:27:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 280 ms, Memory: 85716 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int count = 1;
+
+        while (count != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
