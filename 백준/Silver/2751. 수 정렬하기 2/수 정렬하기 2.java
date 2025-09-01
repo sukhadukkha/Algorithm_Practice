37a20410d7e6f832d17be5447a82dfc6f6bafc8c
sukhadukkha <parkjiho1229@naver.com>
2025-09-01 11:03:57 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1356 ms, Memory: 130764 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,7 +1,10 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
@@ -10,18 +13,21 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        List<Integer> arrList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arrList.add(Integer.parseInt(br.readLine()));
         }
 
-        Arrays.sort(arr);
+        Collections.sort(arrList);
 
-        for (int j : arr) {
-            System.out.println(j);
+        for (int i : arrList) {
+            sb.append(i).append("\n");
         }
+        System.out.println(sb);
+
     }
 }
