88736da07087182cbc3492b8605b3ced56b00953
sukhadukkha <parkjiho1229@naver.com>
2025-09-06 22:36:43 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 112 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,36 +1,39 @@
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
 
 
+    // 규칙: k층 n호 = (k층 n-1호) + (k-1층 n호)
+    // DP 배열 apt[k][n] 미리 채워놓고 답만 출력
     public static void main(String[] args) throws IOException {
 
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
 
         int[][] apt = new int[15][15];
 
-        for (int i = 0; i < 15; i++) {
-            apt[i][1] = 1; // i층 1호는 모두 1명
-            apt[0][i] = i; // 0층 i호는 모두 i명
+        for (int i = 0; i < apt.length; i++) {
+            apt[i][1] = 1;
+            apt[0][i] = i;
         }
 
         for (int i = 1; i < 15; i++) {
-
             for (int j = 2; j < 15; j++) {
                 apt[i][j] = apt[i][j - 1] + apt[i - 1][j];
             }
         }
 
-        // 1층 3호 ->  1+ 0층2호 + 0층 3호 - 1+2+3  --> 1층 2호(0층1호+0층2호)+0층3호
-
-        int t = sc.nextInt();
-
-        for (int i = 0; i < t; i++) {
-            int k = sc.nextInt();
-            int n = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
             System.out.println(apt[k][n]);
         }
+
+
     }
 }
+
