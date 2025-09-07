d07b4dfd5d8aecfa189fda4a67afd3f4ce59520e
sukhadukkha <parkjiho1229@naver.com>
2025-09-07 19:40:21 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,5 +1,8 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -14,20 +17,14 @@ public class Main {
         int B = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        // 목표 지점인 V에서 마지막 날(도착해서 B만큼 떨어지지 않는 날)을 빼주면 V-A
-        int day = (V - A) / (A - B); // N일동안 A-B만큼 올라가고, 마지막 A만큼 더 올라가면 정상 N(A-B)+A = V
+        int day = (V - A) / (A - B);
 
-
-        // “나누어떨어지지 않는다” = 목표 높이 V - A까지 몫만큼의 날 수로는 도달할 수 없다는 뜻, 그래서 하루를 더 더해야 한
         if ((V - A) % (A - B) != 0) {
             day++;
         }
 
         System.out.println(day + 1);
 
-
-
-
-
     }
 }
+
